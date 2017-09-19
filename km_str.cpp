335ac5db44a8f8e0c8c3b0d84dc926631fe702be
wenwenla <wenwenla@outlook.com>
2017-09-19 18:52:53 +0800

    add km_str

---
+++ km_str.cpp
@@ -0,0 +1,35 @@
+typedef long long ll;
+typedef unsigned long long ull;
+
+/*
+ * str_match O(len(str))
+ */
+
+int km_match(char* pattern, char* str) {
+    int plen = strlen(pattern);
+    int slen = strlen(str);
+    int dt = (plen + 1) >> 1;
+    int cnt = 0;
+    ull phash = 0, shash = 0;
+    ull con = 1;
+    for(int i = 0; i < plen; ++i) {
+        phash = ((phash << 7) + pattern[i]);
+        shash = ((shash << 7) + str[i]);
+        con <<= 7;
+    }
+    con >>= 7;
+    for(int i = 0; i < slen - plen + 1; ++i) {
+        if(phash == shash) {
+            bool flag = true;
+            for(int j = 0; j < plen; j += dt) {
+                if(pattern[j] != str[i + j]) {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag) ++cnt;
+        }
+        shash = (((shash - str[i] * con) << 7) + str[i + plen]);
+    }
+    return cnt;
+}
